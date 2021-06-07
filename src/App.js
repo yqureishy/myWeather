2ca03a5187b2163a52d52678d5c19e6ac2ccbd2c
yqureishy <yqureishy1993@gmail.com>
2021-06-07 14:52:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>My Weather</h1>
+    <label htmlFor="username">Username </label>
+      <input id="username" type="text"/>
+    
+    <label htmlFor="">  Password </label>
+    <input id="password" type="password"/>
     </div>
   );
 }
